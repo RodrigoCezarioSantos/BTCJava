134848694565003dab384d681fe9405c19e0be40
Rodrigo <rodrigosan960@gmail.com>
2024-08-04 18:55:15 -0300

    Capturando dados da conta 1.0

--- ContaBanco/ContaTerminal.java
+++ ContaBanco/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class ContaTerminal {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello World");
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Por favor, digite o numero da conta: ");
+    int conta = scan.nextInt();
+
+
+    System.out.println("Por favor, digite o numero da agencia: ");
+    String agencia = scan.next();
+
+
+    System.out.println("Por favor, digite o seu nome: ");
+    String nome = scan.next();
+
+
+    System.out.println("Digite o valor do saldo da sua conta: ");
+    double saldo = scan.nextDouble();
+
+
+
+    System.out.println(String.format("Ola %s obrigado por criar uma conta em nosso banco, sua agencia é: %s, conta: %d e seu saldo %f já está disponível para o saque", nome, agencia, conta, saldo));
+
   }
 }
